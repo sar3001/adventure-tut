593171371b399ab25ad880d3d7e4577760b5a776
Sameer Ramchandran <sar_3001@hotmail.com>
2025-12-23 23:48:33 -0500

    fixed npcs and added text

--- Scenes/NPC/npc.gd
+++ Scenes/NPC/npc.gd
@@ -1,11 +1,21 @@
 extends StaticBody2D
 
-var can_interact: bool = true
+var can_interact: bool = false
+
+@export var dialog_lines: Array[String] = ["Welcome!", "Nice weather we're having!", "Bye now!"]
+var dialog_index: int = 0
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("Interact") and can_interact: #both must be true
-		if $CanvasLayer.visible: #Checks of the boolian is true
-			$CanvasLayer.visible = false
-		else:
+		if dialog_index < dialog_lines.size():
 			$CanvasLayer.visible = true
+			get_tree().paused = true
+		
+			$CanvasLayer/DialogLabel.text = dialog_lines[dialog_index]
+			dialog_index += 1
+			
+		else:
+			$CanvasLayer.visible = false
+			get_tree().paused = false
+			dialog_index = 0
